9bf64b4d23bc64f81078ee4cefe3f8310160acbd
junayed-hassan <junayedhassan102045@gmail.com>
2025-09-22 21:38:05 +0600

    up

--- bin/module_six.dart
+++ bin/module_six.dart
@@ -1,5 +1,32 @@
-import 'package:module_six/module_six.dart' as module_six;
+class Book {
+  String title;
+  String author;
+  double price;
 
-void main(List<String> arguments) {
-  print('Hello world: ${module_six.calculate()}!');
+  // Constructor
+  Book(this.title, this.author, this.price);
+
+  // Method to calculate discounted price
+  double discountedPrice(double discountPercent) {
+    return price - (price * discountPercent / 100);
+  }
+
+  // Method to display book details
+  void display(double discountPercent) {
+    print("Title: $title");
+    print("Author: $author");
+    print("Original Price: $price");
+    print("Discounted Price: ${discountedPrice(discountPercent)}");
+    print("-----------------------------------");
+  }
+}
+
+void main() {
+  // Creating two Book objects
+  Book book1 = Book("Dart Programming", "John Doe", 500);
+  Book book2 = Book("Flutter Development", "Jane Smith", 800);
+
+  // Display details
+  book1.display(10); // 10% discount
+  book2.display(20); // 20% discount
 }
